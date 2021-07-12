b62ef1739378797ec965e7912a5988dc88c5b062
Maksym <sat2151@gmail.com>
2021-07-12 21:37:24 +0300

    Improved Listener

--- script.js
+++ script.js
@@ -16,22 +16,20 @@ for(let i=0; i<SQUARES_NUMBER;i++){
     const square = document.createElement('div')
     square.classList.add('square')
 
-    square.addEventListener('mouseover',()=>{
-        setColor(square)
-    })
-    square.addEventListener('mouseleave',()=>{
-        removeColor(square)
-    })
+    square.addEventListener('mouseover',setColor)
+    square.addEventListener('mouseleave',removeColor)
     board.append(square)
 }
 
-function setColor(element){
+function setColor(event){
+    const element=event.target // our square
     const color=getRandomColor()
     element.style.background=color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element){
+function removeColor(event){
+    const element = event.target
     element.style.backgroundColor='#1d1d1d'
     element.style.boxShadow='0 0 2px #000'
 }
